b181dc6e725489bb88ffbba907858fec7fc1ea4c
Ubuntu <ubuntu@ip-172-31-9-140.ap-south-1.compute.internal>
2024-12-04 18:54:13 +0000

    This is the First Version of SumOfNumbers

---
+++ SumofNumbers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+################################################
+
+#Author : Srivenkatanathan S
+
+#Date : 05-12-2024
+
+#The purspose of this file is to add numbers.
+
+###############################################
+
+
+ValueOne=1
+
+ValueTwo=5
+
+Sum=ValueOne+ValueTwo
+
+echo "The sum of valueone and valuetwo is :" $Sum
+
+
